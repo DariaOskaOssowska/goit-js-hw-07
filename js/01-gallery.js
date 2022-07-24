44fa57578a344bf307cca4f531da0411dfd1855a
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-07-24 17:18:05 +0200

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from './gallery-items.js'
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+const gallery = document.querySelector('.gallery')
+const items = []
+
+galleryItems.forEach(element => {
+	const galleryItem = document.createElement('div')
+	galleryItem.className = 'gallery__item'
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+    galleryImage.className = 'gallery__image'
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute('data-source', element.original)
+    galleryImage.alt = element.description;
+
+	galleryItem.append(galleryLink)
+	galleryLink.append(galleryImage)
+	items.push(galleryItem)
+})
+
+gallery.append(...items)
+
+gallery.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+	}
+
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+    
+    gallery.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+})
+
+
